aef27b0ab7cce1b828e051be9ebe7f143b41e92d
root <397864223@qq.com>
2018-06-26 22:25:44 +0800

    zabbix_httpd dockerfile init

---
+++ zabbix_httpd/update_info.sh
@@ -0,0 +1,6 @@
+target_ip=$(env | grep TARGET_IP | awk -F'=' '{print $2}')
+sed -i "s/localhost/${target_ip}/g" /var/www/html/zabbix/conf/zabbix.conf.php
+while true;do
+    sleep 10
+    echo 1
+done
