22945f5fbbb3cfff46888dd4c82b4bc0fcfcaa6d
root <397864223@qq.com>
2018-07-07 21:26:27 +0800

    update

--- zabbix_httpd/update_info.sh
+++ zabbix_httpd/update_info.sh
@@ -1,6 +1,6 @@
 target_ip=$(env | grep TARGET_IP | awk -F'=' '{print $2}')
 sed -i "s/localhost/${target_ip}/g" /var/www/html/zabbix/conf/zabbix.conf.php
-httpd
+/usr/sbin/httpd
 while true;do
     sleep 10
     echo 1
