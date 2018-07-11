f4bcb0f46f8513ff0ed47e023d2baee0913d1e50
root <397864223@qq.com>
2018-07-11 20:21:12 +0800

    foregroud run httpd

--- zabbix_httpd/update_info.sh
+++ zabbix_httpd/update_info.sh
@@ -1,7 +1,3 @@
 target_ip=$(env | grep TARGET_IP | awk -F'=' '{print $2}')
 sed -i "s/localhost/${target_ip}/g" /var/www/html/zabbix/conf/zabbix.conf.php
-/usr/sbin/httpd
-while true;do
-    sleep 10
-    echo 1
-done
+/usr/sbin/httpd -D FOREGROUND
