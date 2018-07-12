eb510fe880e65835793a3d9c5d803395ca55016c
root <397864223@qq.com>
2018-07-12 23:08:37 +0800

    update check httpd status then start

--- zabbix_httpd/update_info.sh
+++ zabbix_httpd/update_info.sh
@@ -1,3 +1,6 @@
 target_ip=$(env | grep TARGET_IP | awk -F'=' '{print $2}')
 sed -i "s/localhost/${target_ip}/g" /var/www/html/zabbix/conf/zabbix.conf.php
-/usr/sbin/httpd -D FOREGROUND
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+if [ $(ps -ef | grep httpd | wc -l) > 2 ];then  
+    /usr/sbin/httpd -D FOREGROUND
+fi
